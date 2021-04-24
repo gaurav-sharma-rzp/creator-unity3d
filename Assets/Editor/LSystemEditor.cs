0f4724620eb31bb75cd8bc6ed9aea4a9b4b79576
garuasha <garuasha.gs@gmail.com>
2021-04-24 19:51:36 +0530

    adds basic editor setup

---
+++ Assets/Editor/LSystemEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using LSystem;
+[CustomEditor (typeof (Generator))]
+public class LSystemEditor : Editor {
+
+    private Generator generator;
+
+
+    // void OnEnable()
+    // {
+    //     generator = serializedObject.FindProperty("lookAtPoint");
+    // }
+
+    // public override void OnInspectorGUI()
+    // {
+    //     serializedObject.Update();
+    //     EditorGUILayout.PropertyField(lookAtPoint);
+    //     serializedObject.ApplyModifiedProperties();
+    // }
+    // https://www.raywenderlich.com/7751-unity-custom-inspectors-tutorial-getting-started
+   
+    void Awake() { generator =(Generator) target;  }
+
+	public override void OnInspectorGUI() {
+        
+        base.DrawDefaultInspector();
+
+		if (GUILayout.Button ("Generate")) {
+			// mapGen.GenerateMap ();
+            generator.Hi();
+		}
+	}
+}
---
+++ Assets/Scripts/LSystem/Generator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LSystem
+{ 
+    [System.Serializable]
+    public class Generator : MonoBehaviour
+    {
+        [SerializeField]
+        public List<string> initiator;
+        [SerializeField]
+        public List<string> alphabet;
+
+        public int mapWidth;
+        
+
+        public void Hi(){
+            Debug.Log("Hi");
+        }
+    }
+}
