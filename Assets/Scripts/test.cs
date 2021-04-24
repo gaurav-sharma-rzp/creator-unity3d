1a1fdebf931c46fd1b581495e49be442a2d2b381
garuasha <garuasha.gs@gmail.com>
2021-04-24 10:14:32 +0530

    sample networking code

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -7,7 +7,37 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        // https://stackoverflow.com/questions/45470315/unity-3d-call-post-api-with-json-request
+        // public IEnumerator CallLogin(string url, string logindataJsonString)
+        // {
+        //     var request = new UnityWebRequest (url, "POST");
+        //     byte[] bodyRaw = Encoding.UTF8.GetBytes(logindataJsonString);
+        //     request.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
+        //     request.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
+        //     request.SetRequestHeader("Content-Type", "application/json");
+        //     yield return request.SendWebRequest();
+
+        //     if (request.error != null)
+        //     {
+        //         Debug.Log("Erro: " + www.error);
+        //     }
+        //     else
+        //     {
+        //         Debug.Log("All OK");
+        //         Debug.Log("Status Code: " + request.responseCode);
+        //     }
+
+
+
+        //  HttpWebRequest request = 
+        //   (HttpWebRequest)WebRequest.Create(String.Format("http://127.0.0.1:3000/lsystem/expand", 
+        //    CityId, API_KEY));
+        //   HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        //   StreamReader reader = new StreamReader(response.GetResponseStream());
+        //   string jsonResponse = reader.ReadToEnd();
+        //   WeatherInfo info = JsonUtility.FromJson<WeatherInfo>(jsonResponse);
+        //   return info;
     }
 
     // Update is called once per frame
@@ -15,4 +45,5 @@ public class test : MonoBehaviour
     {
         
     }
+    
 }
