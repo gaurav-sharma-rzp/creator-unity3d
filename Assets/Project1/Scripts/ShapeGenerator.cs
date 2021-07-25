c95014393854e020bc051803bb3b20e6774147e3
Gaurav Sharma <gaurav.sharma@razorpay.com>
2021-07-25 14:31:33 +0530

    working terrain generator

--- Assets/Project1/Scripts/ShapeGenerator.cs
+++ Assets/Project1/Scripts/ShapeGenerator.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class ShapeGenerator : MonoBehaviour
 {
+    	const int mapChunkSize = 241;
+	[Range(0,6)]
+	public int levelOfDetail;
+	public float noiseScale;
+
+	public int octaves;
+	[Range(0,1)]
+	public float persistance;
+	public float lacunarity;
+
+	public int seed;
+	public Vector2 offset;
+
+	public float meshHeightMultiplier;
+	public AnimationCurve meshHeightCurve;
+
+	public bool autoUpdate;
+
+	public TerrainType[] regions;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +37,32 @@ public class ShapeGenerator : MonoBehaviour
     }
     public void Generate(){
         Debug.Log("Generate");
+        float[,] noiseMap = Noise.GenerateNoiseMap (mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+		Color[] colourMap = new Color[mapChunkSize * mapChunkSize];
+		for (int y = 0; y < mapChunkSize; y++) {
+			for (int x = 0; x < mapChunkSize; x++) {
+				float currentHeight = noiseMap [x, y];
+				for (int i = 0; i < regions.Length; i++) {
+					if (currentHeight <= regions [i].height) {
+						colourMap [y * mapChunkSize + x] = regions [i].colour;
+						break;
+					}
+				}
+			}
+		}
+        
+        Display display = FindObjectOfType<Display> ();
+		display.DrawMesh (MeshGenerator.GenerateTerrainMesh (noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColourMap (colourMap, mapChunkSize, mapChunkSize));
+
     }
 
    
+}
+
+[System.Serializable]
+public struct TerrainType {
+	public string name;
+	public float height;
+	public Color colour;
 }
